f0321fe8f97fd8a4425578b213862b81730faa40
Mike Cecconello <mikececco2000@gmail.com>
2024-07-20 20:40:57 +0200

    Colo mode switch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
+import { HStack, Image } from '@chakra-ui/react'
 import logo from '../assets/react.svg'
+import ColorModeswitch from './ColorModeswitch'
 
 const Navbar = () => {
   return (
-    <HStack>
+    <HStack justifyContent='space-between' padding='10px'>
       <Image src={logo} />
-      <Text>Navbar</Text>
+      <ColorModeswitch />
     </HStack>
   )
 }
