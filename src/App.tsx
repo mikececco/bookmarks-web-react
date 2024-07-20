9356b458706e7c5a2673da196f0697bd30758296
Mike Cecconello <mikececco2000@gmail.com>
2024-07-20 20:02:49 +0200

    Initialization

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@chakra-ui/react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Button colorScheme='teal' size='xs'>
+        Button
+      </Button>
+    </>
+  )
+}
+
+export default App
