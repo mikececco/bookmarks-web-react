5cd37e59ecec8602e25e9dbd57f03c60e56178c2
Mike Cecconello <mikececco2000@gmail.com>
2024-07-20 20:25:52 +0200

    REsponsive grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,45 @@
-import { Button } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <Button colorScheme='teal' size='xs'>
-        Button
-      </Button>
-    </>
+      <Grid
+        templateAreas={{
+          base: `"header"
+                  "main"
+                  "footer"`,
+          lg: `"header header"
+                "nav main"
+                "nav footer"`,
+          sm: `"header"
+                "main"
+                "footer"`,
+        }}
+        gridTemplateRows={'50px 1fr 30px'}
+        gridTemplateColumns={'150px 1fr'}
+        h='200px'
+        gap='1'
+        color='blackAlpha.700'
+        fontWeight='bold'
+      >
+        <GridItem pl='2' bg='orange.300' area={'header'}>
+          Header
+        </GridItem>
+        <Show above="lg">
+          <GridItem pl='2' bg='pink.300' area={'nav'}>
+            Nav
+          </GridItem>
+        </Show>
+        <GridItem pl='2' bg='green.300' area={'main'}>
+          Main
+        </GridItem>
+        <GridItem pl='2' bg='blue.300' area={'footer'}>
+          Footer
+        </GridItem>
+      </Grid>
+          </>
   )
 }
 
